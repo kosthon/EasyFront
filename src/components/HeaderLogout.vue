98a46d5e4f5cd6523187b642e9b9135165807191
alvertoR <zetoma97@gmail.com>
2020-04-23 15:08:46 -0500

    Vistas de login y registro check

--- src/components/HeaderLogout.vue
+++ src/components/HeaderLogout.vue
@@ -3,7 +3,9 @@
     <header>
         <div class="continer-header">
             <div class="logo">
-                <img src="../assets/header/logo.jpg" />
+                <router-link to="/">
+                    <img src="../assets/header/logo.jpg" />
+                </router-link>
             </div>
             <div class="menu">
                 <nav>
@@ -30,6 +32,7 @@
             </div>
         </div>
     </header>
+    <!--Incio maquetación del Pop-up de login-->
     <div class="bg-modal chao">
         <div class="login1">
             <div class="top">
@@ -75,11 +78,78 @@
             </form>
             <div class="separador-bot"></div>
             <div class="new">
-                <a href="">¿No tienes una cuenta? Regístrate</a>
+                <a @click="register()" id="buttonR">¿No tienes una cuenta? Regístrate</a>
             </div>
         </div>
     </div>
+    <!--Fin maquetación del Pop-up de login-->
     
+    <!--Inicio maquetación del Pop-up de registrar-->
+        <div class="bg-modalAcom">
+            <section class="loginR">
+                <div class="topAcom">
+                    <h2>Regístrate para publicar tu trabajo </h2>
+                    <ul>
+                        <li>
+                            <img src="../assets/acompanamiento/email.png" alt="">
+                        </li>
+                        <li>
+                            <img src="../assets/acompanamiento/facebook.png" alt="">
+                        </li>
+                        <li>
+                            <img src="../assets/acompanamiento/twitter.png" alt="">
+                        </li>
+                    </ul>
+                </div>
+                <div class="separadorAcom">
+                    <div class="leftAcom"></div>
+                    <p>O</p>
+                    <div class="rigthAcom"></div>
+                </div>
+                <form>
+                    <div class="userAcom">
+                        <p>Username</p>
+                        <div class="inputUserAcom">
+                            <img src="../assets/acompanamiento/user.png" alt="">
+                            <input type="text" name="" id="" placeholder="Escribe tu nombre">
+                        </div>
+                    </div>
+                    <div class="userAcom">
+                        <p>Teléfono</p>
+                        <div class="inputUserAcom">
+                            <img src="../assets/acompanamiento/telefono.png" alt="">
+                            <input type="text" name="" id="" placeholder="Escribe tu teléfono">
+                        </div>
+                    </div>
+                    <div class="userAcom">
+                        <p>E-mail</p>
+                        <div class="inputUserAcom">
+                            <img src="../assets/acompanamiento/emailIcon.png" alt="">
+                            <input type="text" name="" id="" placeholder="Escribe tu e-mail">
+                        </div>
+                    </div>
+                    <div class="userAcom">
+                        <p>Contraseña</p>
+                        <div class="inputUserAcom">
+                            <img src="../assets/acompanamiento/password.png" alt="">
+                            <input type="text" name="" id="" placeholder="Escribe tu contraseña">
+                        </div>
+                    </div>
+                    <div class="botonR">
+                        <input type="submit" value="Únete">
+                    </div>
+                    <div class="verificar">
+                        <p>Al registrarte, confirmas que aceptas los Términos y condiciones y la Política de privacidad. </p>
+                    </div>
+                </form>
+                <div class="separador-botAcom"></div>
+                <div class="newAcom">
+                    <a id="buttonAcom" @click="popIn()">¿Ya tíenes una cuenta? Inicía sesión.</a>
+                </div>
+            </section>
+        </div>
+        <!--fin maquetación del Pop-up de registrar-->
+
 </div>
 </template>
 
@@ -88,12 +158,18 @@
 export default {
     date(){
         return{
-            popIsHere:false,
+        
         }
     },
     methods:{
         popIn(){
+            document.querySelector("#buttonAcom").addEventListener('click', function(e){
+                document.querySelector('.bg-modalAcom').style.display = 'none';
+                document.querySelector('.bg-modal').style.display = 'flex';
+            });
+
             document.querySelector("#button").addEventListener('click', function(e){
+                document.querySelector('.bg-modalAcom').style.display = 'none';
                 document.querySelector('.bg-modal').style.display = 'flex';
             });
             var container = document.querySelector('.bg-modal');
@@ -102,6 +178,20 @@ export default {
                 var click = e.target;
                 if(click == container){
                     document.querySelector('.bg-modal').style.display = 'none';
+                }
+            });
+        },
+        register(){
+            document.querySelector("#buttonR").addEventListener('click', function(e){
+                document.querySelector('.bg-modal').style.display = 'none';
+                document.querySelector('.bg-modalAcom').style.display = 'flex';
+            });
+            var container = document.querySelector('.bg-modalAcom');
+
+            document.addEventListener('click',function(e){
+                var click = e.target;
+                if(click == container){
+                    document.querySelector('.bg-modalAcom').style.display = 'none';
                 }
             });
         }
@@ -187,6 +277,7 @@ header nav ul li a div{
 }
 /*Fin estilos del header */
 
+/*Inicio estilos del Pop-up de login */
 
 .bg-modal{
     width: 100%;
@@ -381,6 +472,221 @@ header nav ul li a div{
     font-weight: normal;
     font-size: 13px;
     color: #1A059B;
+    cursor: pointer;
 }
+
+/*Fin estilos del Pop-up de login */
+
+/*Inicio estilos del Pop-Up de registrar */
+.bg-modalAcom{
+    width: 100%;
+    height: 100%;
+    background-color:rgba(0,0,0,0.5);
+    top:0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    display: none;
+    z-index: 99;
+    position: fixed;
+}
+
+
+.loginR{
+    position: relative;
+    width:40%;
+    height: 93%;
+    margin:50px auto;
+    background-color: #F3F1FF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    z-index: 99;
+}
+
+.topAcom{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.topAcom h2{
+    font-family: Inder;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 30px;
+    color: #1A059B;
+    margin-top:36px;
+    text-align: center;
+    height: 47px;
+    margin-bottom: 24px;
+}
+
+
+.topAcom ul{
+    width: 100%;
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin-top:50px;
+    margin-right: 34px;
+
+}
+
+.topAcom ul li{
+    margin-left: 34px;
+    width: 40px;
+    height: 40px;
+    margin-top:-45px;
+}
+
+.separadorAcom{
+    width: 100%;
+    margin-top:20px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+}
+
+.leftAcom{
+    height: 0px;
+    width: 28%;
+    border: 0.5px solid rgba(26, 5, 155, 0.7);
+    margin-left: 25px;
+}
+
+.rigthAcom{
+    height: 0px;
+    width: 28%;
+    border: 0.5px solid rgba(26, 5, 155, 0.7);
+    margin-right: 29px;
+}
+
+.separadorAcom p{
+    margin-left: 29px;
+    margin-right: 25px;
+    font-family: Inder;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 15px;
+    color: #1A059B;
+}
+
+.userAcom{
+    width: 83%;
+    margin: 29px auto;
+    height: 45px;
+    border-bottom:2px solid rgba(26, 5, 155, 0.5);
+    margin-bottom: -5px;
+}
+
+.userAcom p{
+    font-family: Inder;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 15px;
+    color: #1A059B;
+    margin-bottom: 15px;
+}
+
+
+.userAcom img{
+    width: 20px;
+    height: 20px;
+    margin-right: 6px;
+    margin-top: -12px;
+}
+
+.inputUserAcom{
+    display: flex;
+    flex-direction: row;
+}
+
+.inputUserAcom input{
+    border:none;
+    outline: none;
+    background-color: transparent;
+    font-family: Inder;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 26px;
+    display: flex;
+    align-items: center;
+    color: rgba(26, 5, 155, 0.5);
+    margin-top:-13px;
+}
+
+.botonR{
+    width: 90%;
+    height: 50px;
+    margin: 26px auto;
+}
+
+.botonR input{
+    background: linear-gradient(270deg, #5B7CF0 0%, rgba(88, 31, 249, 0.94) 67.59%);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 7px;
+    width: 100%;
+    height: 100%;
+    border:none;
+    font-family: Inder;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 25px;
+    text-align: center;
+    color: #FFFFFF;
+    cursor: pointer;
+}
+
+.botonR input:focus{
+    outline: none;
+}
+
+.verificar{
+    text-align: center;
+    font-family: Inder;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 13px;
+    line-height: 16px;
+    color: #1A059B;
+    margin-top: 18px;
+    margin-bottom:14pxpx ;
+}
+
+
+.verificar p{
+    margin: 0 auto;
+    width: 100%;
+    height: 32px;
+}
+
+
+.separador-botAcom{
+    width: 92%;
+    height: 0px;
+    border: 0.5px solid rgba(26, 5, 155, 0.7);
+    margin: 14px auto;
+}
+
+.newAcom{
+    width: 100%;
+    text-align: center;
+    margin: 0 auto;
+}
+
+.newAcom a{
+    text-decoration: none;
+    font-family: Inder;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 13px;
+    color: #1A059B;
+    cursor:pointer;
+}
+/*Fin estilos del Pop-Up de registrar */
 
 </style>
--- src/components/RegistrarTrabajo.vue
+++ src/components/RegistrarTrabajo.vue
@@ -8,11 +8,11 @@
         </section>
         <section class="form">
             <div class="container-form">
-                <form>
+                <form @submit.prevent="isLogin()">
                     <div class="nombre">
                         <h2>Nombre del trabajo</h2>
                         <input type="text" name="" id="" placeholder="e.j, Derivadas calculo diferencial">
-                        <div class="separador"></div>
+                        <div class="separadorTrabajo"></div>
                     </div>
                     <div class="descripcion">
                         <h2>Decripción del trabajo</h2>
@@ -98,17 +98,18 @@
                             </div>
                         </div>
                     </div>
-                    <div class="boton" v-if="algo == true "  >
-                        <input class="submit" type="submit" value="Publicar tu trabajo">
+                    <div class="boton">
+                        <input id="buttonAcomp" class="submit" type="submit" value="Publicar tu trabajo">
                     </div>
                 </form>
             </div>
+
         </section>
 
-        <!--Maquetación del Pop-up-->
-        <div class="bg-modal">
-            <section class="loginR">
-                <div class="top">
+        <!--Inicio maquetación del Pop-up-->
+        <div class="bg-modalAcomp">
+            <section class="loginRp">
+                <div class="topAcomp">
                     <h2>Regístrate para publicar tu trabajo </h2>
                     <ul>
                         <li>
@@ -122,54 +123,108 @@
                         </li>
                     </ul>
                 </div>
-                <div class="separador">
-                    <div class="left"></div>
+                <div class="separadorAcomp">
+                    <div class="leftAcomp"></div>
                     <p>O</p>
-                    <div class="rigth"></div>
+                    <div class="rigthAcomp"></div>
                 </div>
                 <form>
-                    <div class="user">
+                    <div class="userAcomp">
                         <p>Username</p>
-                        <div class="inputUser">
+                        <div class="inputUserAcomp">
                             <img src="../assets/acompanamiento/user.png" alt="">
                             <input type="text" name="" id="" placeholder="Escribe tu nombre">
                         </div>
                     </div>
-                    <div class="user">
+                    <div class="userAcomp">
                         <p>Teléfono</p>
-                        <div class="inputUser">
+                        <div class="inputUserAcomp">
                             <img src="../assets/acompanamiento/telefono.png" alt="">
                             <input type="text" name="" id="" placeholder="Escribe tu teléfono">
                         </div>
                     </div>
-                    <div class="user">
+                    <div class="userAcomp">
                         <p>E-mail</p>
-                        <div class="inputUser">
-                            <img src="../assets/acompanamiento/email.png" alt="">
+                        <div class="inputUserAcomp">
+                            <img src="../assets/acompanamiento/emailIcon.png" alt="">
                             <input type="text" name="" id="" placeholder="Escribe tu e-mail">
                         </div>
                     </div>
-                    <div class="user">
+                    <div class="userAcomp">
                         <p>Contraseña</p>
-                        <div class="inputUser">
+                        <div class="inputUserAcomp">
                             <img src="../assets/acompanamiento/password.png" alt="">
                             <input type="text" name="" id="" placeholder="Escribe tu contraseña">
                         </div>
                     </div>
-                    <div class="botonR">
+                    <div class="botonRp">
                         <input type="submit" value="Únete">
                     </div>
                     <div class="verificar">
                         <p>Al registrarte, confirmas que aceptas los Términos y condiciones y la Política de privacidad. </p>
                     </div>
                 </form>
-                <div class="separador-bot"></div>
-                <div class="new">
-                    <a href="">¿Ya tíenes una cuenta? Inicía sesión.</a>
+                <div class="separador-botAcomp"></div>
+                <div id="buttonAcompLogin" @click="login()" class="newAcomp">
+                    <a>¿Ya tíenes una cuenta? Inicía sesión.</a>
                 </div>
             </section>
         </div>
         <!--fin maquetación del Pop-up-->
+
+            <!--Incio maquetación del Pop-up de login-->
+    <div class="bg-modalAl chao">
+        <div class="login1Al">
+            <div class="topAl">
+                <h2>Login</h2>
+                <ul>
+                    <li>
+                        <img src="../assets/header/email.png" alt="">
+                    </li>
+                    <li>
+                        <img src="../assets/header/facebook.png" alt="">
+                    </li>
+                    <li>
+                        <img src="../assets/header/twitter.png" alt="">
+                    </li>
+                </ul>
+            </div>
+            <div class="separadorAl">
+                <div class="leftAl"></div>
+                <p>O</p>
+                <div class="rigthAl"></div>
+            </div>
+            <form>
+                <div class="userAl">
+                    <p>Username</p>
+                    <div class="inputUserAl">
+                        <img src="../assets/header/user.png" alt="">
+                        <input type="text" name="" id="" placeholder="Escribe tu nombre">
+                    </div>
+                </div>
+                <div class="userAl">
+                    <p>Password</p>
+                    <div class="inputUserAl">
+                        <img src="../assets/header/password.png" alt="">
+                        <input type="text" name="" id="" placeholder="Escribe tu contraseña">
+                    </div>
+                </div>
+                <div class="forgetAl">
+                    <a href="">¿Olvidaste tu contraseña?</a>
+                </div>
+                <div class="botonAl">
+                    <input type="submit" value="Ingresar">
+                </div>
+            </form>
+            <div class="separador-botAl"></div>
+            <div class="newAl">
+                <a @click="popIn()" id="buttonLoginAcom">¿No tienes una cuenta? Regístrate</a>
+            </div>
+        </div>
+    </div>
+    <!--Fin maquetación del Pop-up de login-->
+
+
     </main>
 </template>
 
@@ -181,17 +236,42 @@ export default {
         }
     },
     methods:{
-        popIn(e){
-            e.preventDefault();
-            document.querySelector("#button").addEventListener('click', function(e){
-                document.querySelector('.bg-modal').style.display = 'flex';
+        popIn(){
+            
+            document.querySelector("#buttonLoginAcom").addEventListener('click', function(e){
+                document.querySelector('.bg-modalAl').style.display = 'none';   
+                document.querySelector('.bg-modalAcomp').style.display = 'flex';    
             });
-            var container = document.querySelector('.bg-modal');
+           
+
+            document.querySelector("#buttonAcomp").addEventListener('click', function(e){
+                document.querySelector('.bg-modalAcomp').style.display = 'flex';    
+            });
+
+            var container      = document.querySelector('.bg-modalAcomp');
 
             document.addEventListener('click',function(e){
                 var click = e.target;
                 if(click == container){
-                    document.querySelector('.bg-modal').style.display = 'none';
+                    document.querySelector('.bg-modalAcomp').style.display = 'none';
+                }
+            });            
+        },
+        isLogin(){
+            this.popIn();
+        },
+        login(){
+            document.querySelector("#buttonAcompLogin").addEventListener('click', function(e){
+                document.querySelector('.bg-modalAcomp').style.display = 'none';    
+                document.querySelector('.bg-modalAl').style.display = 'flex';
+            });
+
+            var container = document.querySelector('.bg-modalAl');
+
+            document.addEventListener('click',function(e){
+                var click = e.target;
+                if(click == container){
+                    document.querySelector('.bg-modalAl').style.display = 'none';
                 }
             });
         }
@@ -298,7 +378,7 @@ form{
     border:none;
 }
 
-.separador{
+.separadorTrabajo{
     width: 98%;
     margin:0 auto;
     border: 1px solid rgba(26, 5, 155, 0.8);
@@ -608,41 +688,43 @@ form{
 }
 /*Fin estilos registro trabajo */
 
-/*Inicio estilos Pop-Up */
-.bg-modal{
+/*Inicio estilos Pop-Up de registrar*/
+.bg-modalAcomp{
     width: 100%;
     height: 100%;
     background-color:rgba(0,0,0,0.5);
-    position: absolute;
     top:0;
     display: flex;
     justify-content: center;
     align-items: center;
     display: none;
+    position: fixed;
 }
 
 
-.loginR{
+.loginRp{
+    position: relative;
     width:40%;
-    height: 800px;
+    height: 93%;
     margin:50px auto;
     background-color: #F3F1FF;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 10px;
+    z-index: 99;
 }
 
-.top{
+.topAcomp{
     width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
 }
 
-.top h2{
+.topAcomp h2{
     font-family: Inder;
     font-style: normal;
     font-weight: normal;
-    font-size: 40px;
+    font-size: 30px;
     color: #1A059B;
     margin-top:36px;
     text-align: center;
@@ -651,7 +733,7 @@ form{
 }
 
 
-.top ul{
+.topAcomp ul{
     width: 100%;
     list-style: none;
     display: flex;
@@ -662,36 +744,37 @@ form{
 
 }
 
-.top ul li{
+.topAcomp ul li{
     margin-left: 34px;
     width: 40px;
     height: 40px;
+    margin-top:-45px;
 }
 
-.separador{
+.separadorAcomp{
     width: 100%;
-    margin-top:40px;
+    margin-top:20px;
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: center;
 }
 
-.left{
+.leftAcomp{
     height: 0px;
     width: 28%;
     border: 0.5px solid rgba(26, 5, 155, 0.7);
     margin-left: 25px;
 }
 
-.rigth{
+.rigthAcomp{
     height: 0px;
     width: 28%;
     border: 0.5px solid rgba(26, 5, 155, 0.7);
     margin-right: 29px;
 }
 
-.separador p{
+.separadorAcomp p{
     margin-left: 29px;
     margin-right: 25px;
     font-family: Inder;
@@ -701,54 +784,58 @@ form{
     color: #1A059B;
 }
 
-.user{
+.userAcomp{
     width: 83%;
     margin: 29px auto;
-    height: 59px;
+    height: 45px;
     border-bottom:2px solid rgba(26, 5, 155, 0.5);
+    margin-bottom: -5px;
 }
 
-.user p{
+.userAcomp p{
     font-family: Inder;
     font-style: normal;
     font-weight: normal;
     font-size: 15px;
     color: #1A059B;
+    margin-bottom: 15px;
 }
 
 
-.user img{
+.userAcomp img{
+    width: 20px;
+    height: 20px;
     margin-right: 6px;
-    margin-top: 6px;
+    margin-top: -12px;
 }
 
-.inputUser{
+.inputUserAcomp{
     display: flex;
     flex-direction: row;
 }
 
-.inputUser input{
+.inputUserAcomp input{
     border:none;
     outline: none;
     background-color: transparent;
     font-family: Inder;
     font-style: normal;
     font-weight: normal;
-    font-size: 21px;
+    font-size: 18px;
     line-height: 26px;
     display: flex;
     align-items: center;
     color: rgba(26, 5, 155, 0.5);
-    margin-top: 6px;
+    margin-top:-13px;
 }
 
-.botonR{
+.botonRp{
     width: 90%;
     height: 50px;
-    margin: 26px auto;
+    margin: 45px auto;
 }
 
-.botonR input{
+.botonRp input{
     background: linear-gradient(270deg, #5B7CF0 0%, rgba(88, 31, 249, 0.94) 67.59%);
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 7px;
@@ -764,7 +851,7 @@ form{
     cursor: pointer;
 }
 
-.botonR input:focus{
+.botonRp input:focus{
     outline: none;
 }
 
@@ -776,8 +863,7 @@ form{
     font-size: 13px;
     line-height: 16px;
     color: #1A059B;
-    margin-top: 18px;
-    margin-bottom:14pxpx ;
+    margin-top: -20px;
 }
 
 
@@ -788,20 +874,21 @@ form{
 }
 
 
-.separador-bot{
+.separador-botAcomp{
     width: 92%;
     height: 0px;
     border: 0.5px solid rgba(26, 5, 155, 0.7);
     margin: 14px auto;
 }
 
-.new{
+.newAcomp{
     width: 100%;
     text-align: center;
     margin: 0 auto;
+    cursor: pointer;
 }
 
-.new a{
+.newAcomp a{
     text-decoration: none;
     font-family: Inder;
     font-style: normal;
@@ -809,6 +896,206 @@ form{
     font-size: 13px;
     color: #1A059B;
 }
-/*Fin estilos Pop-Up */
+/*Fin estilos Pop-Up de registrar*/
+
+/*Inicio estilos del Pop-up de login */
+
+.bg-modalAl{
+    width: 100%;
+    height: 100%;
+    background-color:rgba(0,0,0,0.5);
+    position: fixed;
+    top:0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    display: none;
+    z-index: 99;
+}
+
+
+.login1Al{
+    position: relative;
+    width: 30%;
+    height: 650px;
+    margin:0px auto;
+    background-color: #F3F1FF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+}
+
+.topAl{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.topAl h2{
+    font-family: Inder;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 50px;
+    color: #1A059B;
+    margin-top:36px;
+    text-align: center;
+    height: 47px;
+    margin-left: 9px;
+}
+
+.topAl ul{
+    width: 100%;
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin-top:50px;
+    margin-right: 34px;
+}
+
+.topAl ul li{
+    margin-left: 34px;
+    width: 40px;
+    height: 40px;
+}
+
+.separadorAl{
+    width: 100%;
+    margin-top:40px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.leftAl{
+    height: 0px;
+    width: 39%;
+    border: 0.5px solid rgba(26, 5, 155, 0.7);
+    margin-left: 25px;
+}
+
+.rigthAl{
+    height: 0px;
+    width: 39%;
+    border: 0.5px solid rgba(26, 5, 155, 0.7);
+    margin-right: 29px;
+}
+
+.separadorAl p{
+    margin-left: 29px;
+    margin-right: 25px;
+    font-family: Inder;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 15px;
+    color: #1A059B;
+}
+
+.userAl{
+    width: 84%;
+    margin: 29px auto;
+    height: 59px;
+    border-bottom:2px solid rgba(26, 5, 155, 0.5);
+}
+
+.userAl p{
+    font-family: Inder;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 15px;
+    color: #1A059B;
+}
+
+
+.userAl img{
+    margin-right: 6px;
+    margin-top: 6px;
+}
+.inputUserAl{
+    display: flex;
+    flex-direction: row;
+}
+
+.inputUserAl input{
+    border:none;
+    outline: none;
+    background-color: transparent;
+    font-family: Inder;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 21px;
+    line-height: 26px;
+    display: flex;
+    align-items: center;
+    color: rgba(26, 5, 155, 0.5);
+    margin-top: 6px;
+}
+
+.forgetAl{
+    width: 83%;
+    margin: -18px auto;
+    text-align: right;
+}
+
+.forgetAl a{
+    text-decoration: none;
+    font-family: Inder;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 13px;
+    color: #1A059B;
+}
+
+.botonAl{
+    width: 90%;
+    height: 50px;
+    margin: 60px auto;
+}
+
+.botonAl input{
+    background: linear-gradient(270deg, #5B7CF0 0%, rgba(88, 31, 249, 0.94) 67.59%);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 7px;
+    width: 100%;
+    height: 100%;
+    border:none;
+    font-family: Inder;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 25px;
+    text-align: center;
+    color: #FFFFFF;
+    cursor: pointer;
+}
+
+.botonAl input:focus{
+    outline: none;
+}
+
+.separador-botAl{
+    width: 92%;
+    height: 0px;
+    top: 580px;
+    border: 0.5px solid rgba(26, 5, 155, 0.7);
+    margin: -31px auto;
+}
+
+.newAl{
+    width: 100%;
+    text-align: center;
+    margin: 50px auto;
+}
+
+.newAl a{
+    text-decoration: none;
+    font-family: Inder;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 13px;
+    color: #1A059B;
+    cursor: pointer;
+}
+
+/*Fin estilos del Pop-up de login */
 
 </style>
